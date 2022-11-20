4d636853ad8ea206a22c1ba15585c5407b2455e2
0xgeeb <0xgeeb@gmail.com>
2022-11-19 19:01:46 -0500

    fixed error

--- src/Disperse.sol
+++ src/Disperse.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-interface IDERC721 {
+interface IERC721 {
   function safeTransferFrom(address from, address to, uint256 tokenId) external;
 }
 
 contract Disperse {
 
-  function disperseTokens(IDERC721 token, address[] calldata recipients, uint256[] calldata tokenIds) public {
+  function disperseTokens(IERC721 token, address[] calldata recipients, uint256[] calldata tokenIds) public {
     require(recipients.length == tokenIds.length, "invalid amount of recipients/tokenIds");
     for (uint256 i; i < recipients.length; i++) {
       token.safeTransferFrom(msg.sender, recipients[i], tokenIds[i]);
